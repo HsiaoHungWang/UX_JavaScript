bf6a8a5c359c3de72ef2d576f705995c1b8d5f93
HsiaoHungWang <wang@ispan.com.tw>
2025-08-25 09:32:53 +0800

    計算BMI的function

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,27 @@ function tempatureConvert(degree, type) {
 
     return result;
 }
+
+//BMI = 體重(公斤)/身高(公尺)的平方
+function calcBMI(height, w) {
+    const h = height / 100; //公尺
+    //const w = weight; //公斤
+    const bmi = w / (h * h); // h ** 2
+    // console.log(bmi.toFixed(2));
+
+    let message = '';
+    if (bmi < 18.5) {
+        message = '過輕';
+    } else if (bmi < 24) {
+        message = '正常'
+    } else if (bmi < 27) {
+        message = '過重'
+    } else {
+        message = '請重新輸入'
+    }
+
+    // console.log(message);
+    //[]、{key:value, key:value}
+    return { "bmi": bmi.toFixed(2), "message": message };
+}
+
