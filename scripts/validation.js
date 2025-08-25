e6178706cad1dd589028a0dda950c05a4f3f0ca1
HsiaoHungWang <wang@ispan.com.tw>
2025-08-25 14:58:26 +0800

    檢查副檔名

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,3 +9,14 @@ const checkMail = mail => {
 
     return isValid;
 }
+
+const checkImageFile = fileName => {
+    const imgExtensions = ['jpg', 'png', 'webp'];
+    //split() 把字串轉成陣列
+    //trim() 把字串的前後空白去掉
+    //toLowerCase() 把字串轉成小寫
+    const extensions = fileName.split('.')[1].trim().toLowerCase();
+
+    return imgExtensions.includes(extensions);
+
+}
